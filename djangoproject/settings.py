f09d0b4603d073a52fe445c4f149e7018c81bb08
Mark Packham <markpackham1@gmail.com>
2018-11-27 17:26:00 +0000

    Can't progress further on my machine, too many issues with "pip install mysqlclient" stopping at this point https://youtu.be/D6esTdOLXh4?t=1534

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
+''' DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+ '''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':''
     }
 }
 
